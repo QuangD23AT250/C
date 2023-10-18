300f47a4f7ac47db8ed38c79a99e9a660c6d55b7
quangcoc <quangcoc69@gmail.com>
2023-10-19 00:38:45 +0700

    Create dondieu.c

---
+++ dondieu.c
@@ -0,0 +1,99 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	long long a,b,c,d;
+	double x1,x2;
+	scanf("%lld%lld%lld%lld",&a,&b,&c,&d);
+	
+	
+	if (a != 0 && b!=0 && c!=0)
+	{
+	
+		long long b2= 2*b;
+		long long a2= 3*a;
+		
+		
+		// delta y'
+	    double delta = b2 * b2 - 4 * a2 * c;
+
+  
+    	if (delta > 0) 
+		{
+	        
+	        x1 = (-b2 + sqrt(delta)) / (2 * a2);
+	        x2 = (-b2 - sqrt(delta)) / (2 * a2);
+	        printf("x1 = %.3lf\n", x1);
+	        printf("x2 = %.3lf\n", x2);
+        
+	        if ( 6*a*x1 +2*b > 0 )
+	        { 
+	          double cty= a*pow(x1,3)+b*pow(x1,2)+c*x1+d;
+	          double cdy= a*pow(x2,3)+b*pow(x2,2)+c*x2+d;
+	         printf("CD : (%.3lf, %.3lf)\n",x2,cdy);
+	         printf("CT : (%.3lf, %.3lf)",x1,cty);
+			}
+			if ( 6*a*x1 +2*b < 0 )
+	        { 
+	          double cty= a*pow(x2,3)+b*pow(x2,2)+c*x2+d;
+	          double cdy= a*pow(x1,3)+b*pow(x1,2)+c*x1+d;
+	         printf("CD : (%.3lf, %.3lf)\n",x1,cdy);
+	         printf("CT : (%.3lf, %.3lf)",x2,cty);
+			}
+        
+    	} else {
+        
+        printf("NONE");
+        if (a > 0) printf("+");
+        else printf("-");
+        
+    	}
+	}
+		
+	
+    else if ( b!= 0 && c!=0)
+    {
+    	double k = -c/(2*b);
+    	
+    	if ( b > 0 ) 
+    	{
+    		double y= b*k*k+c*k+d;
+    		printf("CT: (%.3lf, %.3lf)", k,y);
+    		
+		}
+		else
+		{
+		  double y= b*k*k+c*k+d;
+    		printf("CD: (%.3lf, %.3lf)", k,y);	
+			
+		}
+	 	
+	}
+	else if (c!=0)
+	{
+		printf("NONE\n");
+		if (c>0) printf("+");
+		else printf("-");
+		
+	}
+	else 
+	{
+		float z= d;
+		printf("%.3lf",z);
+	}
+   
+   	
+				
+
+	
+
+	
+		
+		
+
+	
+	
+	
+	
+return 0;	
+}
