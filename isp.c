fecd38dbd428e040e1d1495a53b0070d6c8efe4b
quangcoc <quangcoc69@gmail.com>
2023-10-18 14:16:48 +0700

    Update isp.c

--- isp.c
+++ isp.c
@@ -6,16 +6,15 @@
    
    
    int ax,ay,bx,by,cx,cy; 
-   double AB,AC,BC,cos;
+   double AB,AC,BC,z,c;
    scanf("%d%d%d%d%d%d",&ax,&ay,&bx,&by,&cx,&cy);
    AB= sqrt(pow(bx-ax,2)+pow(by-ay,2));
    BC= sqrt(pow(cx-bx,2)+pow(cy-by,2));
    AC= sqrt(pow(cx-ax,2)+pow(cy-ay,2));
  
-  printf("%d%d%d",AB,AC,BC);
   
-  cos=(pow(AB,2)+pow(AC,2)-pow(BC,2))/(2*AB*AC);
-  
- 
+  z=(pow(AB,2)+pow(AC,2)-pow(BC,2))/(2*AB*AC);
+  c=acos(z);
+  printf("%.2lf", c);
  return 0;
  }
