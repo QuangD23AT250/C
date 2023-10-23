ded1fdfec39c1eb29b05081f06c99c5215d8e3dc
quangcoc <quangcoc69@gmail.com>
2023-10-23 19:37:58 +0700

    Create vonglap.c

---
+++ vonglap.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+ int main()
+ {
+ 	double n,max1=0,max2=0,ta=0,tb=0,tba,tbb;
+ 	scanf("%lf",&n);
+ 	for (int i=1;i<=n;i++)
+ 	{ 
+ 	   double a,b;
+ 		scanf("%lf%lf",&a,&b);
+ 		if(a>max1) max1=a;
+ 	    if(b>max2) max2=b;
+ 		
+ 		ta= ta+a;
+		tb= tb+b; 
+	 }
+	 tba=ta/n;
+	 tbb=tb/n;
+	 printf("%lf %lld %.2lf %.2lf",max1,max2,tba,tbb);
+	 return 0;
+ }
+ 
+ 
